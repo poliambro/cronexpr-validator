c3718e9093918d7357b87d521aaff8a8c809148b
PolianaNeves <poliana.ambrosio.campos@gmail.com>
2022-06-20 16:15:41 -0300

    add ci check for main branch

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='cronexpr-translator',
+    name='cronexpr-validator',
     version='1.0.2',
     license='MIT',
     author="poliambro",
